7d925cfea104980c5ea2b99ff368fd8b30e4e7fe
lcoelho3412 <leandrocoelho3@yahoo.com>
2023-03-28 12:44:19 -0300

    Adds get_max_salary function

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,22 +1,15 @@
 from typing import Union, List, Dict
+from src.insights.jobs import read
 
 
 def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
+    data = read(path)
+    highest_salary = []
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    for job in data:
+        if job["max_salary"] and job["max_salary"] != "invalid":
+            highest_salary.append(int(job["max_salary"]))
+    return max(highest_salary)
 
 
 def get_min_salary(path: str) -> int:
